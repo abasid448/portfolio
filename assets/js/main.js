f09cfd61091621f5accdef1b9db429c42c8dddea
abasid448 <abasid448@gmail.com>
2022-09-27 11:11:29 +0530

    secondcommit

---
+++ assets/js/main.js
@@ -0,0 +1,342 @@
+/**
+* Template Name: DevFolio - v4.9.0
+* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Intro type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
+
+// Validation
+const gform = document.getElementById('gform')
+const username = document.getElementById('username')
+const number = document.getElementById('number')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const content = document.getElementById('content')
+
+
+function setError(element, message){
+    const formgroup = element.parentElement;
+    const errorDisplay = formgroup.querySelector('.error')
+
+    errorDisplay.innerText = message
+    formgroup.classList.add('error');
+    formgroup.classList.remove('success');
+}
+
+function setSuccess(element){
+    const formgroup = element.parentElement;
+    const errorDisplay = formgroup.querySelector('.error');
+
+
+    errorDisplay.innerText = '';
+    formgroup.classList.add('success');
+    formgroup.classList.remove('error');
+}
+
+function isValidNumber(number){
+    const re = /^[0-9]{10}$/;
+    return re.test(String(number));
+}
+
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function submitForm() {
+    let gform = document.getElementById('gform');
+    gform.querySelector("button#submit-btn-btn").click();
+    // console.log(gform);
+}
+
+function validateInputs(){
+    const usernameValue = username.value.trim();
+    const numberValue = number.value.trim();
+    const emailValue = email.value.trim();
+    const subjectValue = subject.value.trim();
+
+    var flag = 2;
+
+    // Name
+    if (usernameValue == '') {
+        setError(username, 'This field cannot be empty');
+        flag = 1;
+    } else if (usernameValue.length <= 2) {
+        setError(username, 'Enter name correctly');
+        flag = 1;
+    } else {
+        setSuccess(username);
+    }
+
+    // Number
+    if (numberValue == '') {
+        setError(number, 'This field cannot be empty');
+        flag = 1;
+    } else if (numberValue.length !== 10) {
+        setError(number, 'Number must be 10 characters');
+        flag = 1;
+    } else if (!isValidNumber(numberValue)) {
+        setError(number, 'Enter number correctly');
+        flag = 1;
+    }
+    else {
+        setSuccess(number);
+    }
+
+    // Email
+    if (emailValue == '') {
+        setError(email, 'This field cannot be empty');
+        flag = 1;
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Email is not valid');
+        flag = 1;
+    } else {
+        setSuccess(email);
+    }
+
+    // subject
+    if (subjectValue === '') {
+        setError(subject, 'This field cannot be empty');
+        flag = 1;
+    } else if (subjectValue.length < 20) {
+        setError(subject, 'Subject should be 20 characters or more');
+        flag = 1;
+    } else {
+        setSuccess(subject);
+    }
+
+
+
+    if (flag == 2) {
+        submitForm();
+    }
+
+}
